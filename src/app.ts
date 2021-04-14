b292114bac8c8c1e7d85f53dc9de2e52294a2070
Saleh Alibrahim <s.albrahim@outlook.com>
2021-04-14 06:02:17 +0300

    feat: 🎸 Connect to SQL server database

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+import connectDB from './config/db';
+
+// Load config
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const app: Application = express();
 
@@ -10,8 +19,5 @@ app.get('/', (req, res, next) => {
 const PORT: number = (process.env.PORT as any) || 3000;
 
 app.listen(PORT, () => {
-    console.log(
-        `Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`
-            .yellow.bold,
-    );
+    console.log(`Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`.yellow.bold);
 });
--- src/config/db.ts
+++ src/config/db.ts
@@ -2,11 +2,13 @@ const { Sequelize } = require('sequelize');
 
 const connectDB = async () => {
     try {
-        const sequelize = new Sequelize('database', 'username', 'password', {
+        const sequelize = new Sequelize('master', 'sa', '123', {
             host: 'localhost',
+            port: 1434,
             dialect: 'mssql',
         });
         await sequelize.authenticate();
+
         console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
