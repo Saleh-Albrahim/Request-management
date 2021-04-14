706d55a07df20f4166b7567373446081400d4425
Saleh Alibrahim <s.albrahim@outlook.com>
2021-04-14 06:20:31 +0300

    feat: 🎸 Install security packages

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,32 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 import 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-
 import connectDB from './config/db';
+import hpp from 'hpp';
+import cors from 'cors';
+import helmet from 'helmet';
 
 // Load config
 dotenv.config({ path: './config/config.env' });
 
+// Init the express server
+const app: Application = express();
+
+// Connect to the database
 connectDB();
 
-const app: Application = express();
+app.use(express.json());
+
+app.use(morgan('short'));
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Set security headers
+app.use(helmet());
 
 app.get('/', (req, res, next) => {
     res.json('Hey');
--- src/config/db.ts
+++ src/config/db.ts
@@ -2,14 +2,14 @@ const { Sequelize } = require('sequelize');
 
 const connectDB = async () => {
     try {
-        const sequelize = new Sequelize('master', 'sa', '123', {
+        const sequelize = new Sequelize('request', 'sa', '123', {
             host: 'localhost',
             port: 1434,
             dialect: 'mssql',
         });
         await sequelize.authenticate();
 
-        console.log('Connection has been established successfully.');
+        console.log('Connection has been established successfully'.green.bold);
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
